7ac0efdf02665b3a8d84bc01e29013ffa6366861
xellossmail <xellossmail@gmail.com>
2018-07-28 19:09:11 +0900

    server.js 로직 간략화 라우터 적용하여 파일별 소스 분기 mysql 연결 모듈 별도로 생성하여 각각의 로직에서 쓸때마다 연결하도록 구성(반드시 close해줘야함)

---
+++ NodejsWebApp/modules/mysql.js
@@ -0,0 +1,31 @@
+﻿module.exports = function () {
+    const mysql = require('mysql');
+    var conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'xelloss',
+        password: 'test1234',
+        database: 'tasty',
+    });
+    conn.close = function () {
+        console.log('db closed');
+        conn.end();
+    }
+    try {
+        conn.connect();
+        //conn.query('SELECT * FROM code', function (err, rows, fields) {
+        //    if (err) {
+        //        console.log(err);
+        //    } else {
+        //        console.log('rows', rows);
+        //        console.log('fileds', fields);
+        //    }
+        //});
+        //conn.end();
+        console.log('db connected');
+        return conn;
+    }
+    catch (ex) {
+        console.log(ex);
+        return null;
+    }
+};
