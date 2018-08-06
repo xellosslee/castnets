2b047e90173b4ec30402f00560d011d55bf34cf5
xellossmail <xellossmail@gmail.com>
2018-08-06 17:24:04 +0900

    회원가입, 로그인, 토큰 정상체크 디비 코드 추가, 결과코드 추가

--- NodejsWebApp/config/code insert.sql
+++ NodejsWebApp/config/code insert.sql
@@ -1,13 +1,28 @@
 INSERT INTO `code` VALUES (10000, NULL, '유저정보', '유저정보');
+
 INSERT INTO `code` VALUES (10100, 10000, '성별', '미설정');
-INSERT INTO `code` VALUES (10101, 10000, '남성', '남성');
-INSERT INTO `code` VALUES (10102, 10000, '여성', '여성');
+INSERT INTO `code` VALUES (10101, 10100, '남성', '남성');
+INSERT INTO `code` VALUES (10102, 10100, '여성', '여성');
+
 INSERT INTO `code` VALUES (10200, 10000, '유저상태', '미설정');
 INSERT INTO `code` VALUES (10201, 10200, '가입유저', '가입유저');
 INSERT INTO `code` VALUES (10202, 10200, '인증유저', '인증유저');
 INSERT INTO `code` VALUES (10203, 10200, '휴면유저', '휴면유저');
 INSERT INTO `code` VALUES (10204, 10200, '관리자', '관리자');
 
+INSERT INTO `code` VALUES (90000, NULL, '접속정보', '접속정보');
+
+INSERT INTO `code` VALUES (90100, 90000, '접속경로', '미설정');
+INSERT INTO `code` VALUES (90101, 90100, '접속경로', '안드로이드');
+INSERT INTO `code` VALUES (90102, 90100, '접속경로', '애플');
 
 -- 관리자(이근석) 계정 생성
 INSERT INTO userinfo VALUES (NULL, 'xelloss@gmail.com', 'xelloss', NULL, 10101, 10204, NULL, NULL, CURRENT_TIMESTAMP, NULL);
+
+-- 로그인테스트
+SET @a = '';
+CALL userlogin('xelloss@gmail.com', 'k4NLEHm2R+abZ28HJICgt/IEsIB5rrkXb7NfCebY9+pUachbFxC0cNfc0TVNA1q/KLkN6BwByAN7FwB5Wwh/nA==', 90101, @a);
+SELECT @a;
+
+CALL usersessioncheck('B6325A39E2802F41EBDE43D776241A5B');
+
--- NodejsWebApp/routes/user.js
+++ NodejsWebApp/routes/user.js
@@ -12,7 +12,7 @@
     //    next();
     //});
     /** 회원가입
-     * req : phone, email(둘중 하나만 보내면 됨), pass
+     * req : phone, email(둘중 하나만 보내면 됨), pass, loginpath(구글:90101 애플:90102)
      * res : resultcode 결과값과 token값 json으로 리턴
      */ 
     route.post('/join', function (req, res, next) {
@@ -26,23 +26,137 @@
                 crypto.pbkdf2(req.body.pass, salt, 100000, 64, 'sha512', (err, key) => {
                     pass = key.toString('base64');
 
-                    var text = 'CALL userjoin(\'' + req.body.phone + '\',\'' + req.body.email + '\',\'' + pass + '\',\'' + salt + '\')';
+                    var text = 'SET @token = \'\';\
+                        CALL userjoin(\'' + (req.body.phone === undefined ? '' : req.body.phone) + '\',\''
+                        + (req.body.email === undefined ? '' : req.body.email) + '\',\''
+                        + pass + '\',\'' + salt + '\',' + req.body.loginpath + ', @token);'
+                        + 'SELECT @token;';
+
+                    console.log(text);
                     conn.query(text, function (err, rows) {
                         if (err) {
+                            res.json('{"resultcode":' + resultcode.Failed + ', "token":""}');
+                            conn.close();
                             throw err;
                         }
                         else {
-                            res.json('{"resultcode":' + resultcode.SUCCESS + '}');
+                            console.log('Issued access token : ' + rows[2][0]['@token']);
+                            res.json('{"resultcode":' + resultcode.Success + ', "token":"' + rows[2][0]['@token'] + '"}');
                             conn.close();
-
                         }
                     });
                 });
             });
         }
         catch (e) {
-            res.json('{"resultcode":' + resultcode.FAILED + '}');
+            res.json('{"resultcode":' + resultcode.Failed + '}');
             conn.close();
+            throw err;
+        }
+    });
+    /** 유저로그인
+     * req : phone, email(둘중 하나만 보내면 됨), pass, loginpath(구글:90101 애플:90102)
+     * res : resultcode 결과값과 token값 json으로 리턴
+     */
+    route.post('/login', function (req, res, next) {
+        console.log(req.body);
+
+        var conn = require('../modules/mysql.js')();
+        try {
+            var pass, salt;
+            var sql = 'CALL usergetsalt(\'' + (req.body.phone === undefined ? req.body.email : req.body.phone) + '\');';
+
+            conn.query(sql, function (err, rows) {
+                if (err) {
+                    res.json('{"resultcode":' + resultcode.Failed + ', "token":""}');
+                    conn.close();
+                    throw err;
+                }
+                else {
+                    if (rows[0].length <= 0) {
+                        res.json('{"resultcode":' + resultcode.NotExistsAccount + ', "token":""}');
+                        conn.close();
+                        return;
+                    }
+                    console.log('get salt : ' + rows[0][0]['salt']);
+                    salt = rows[0][0]['salt'];
+
+                    crypto.pbkdf2(req.body.pass, salt, 100000, 64, 'sha512', (err, key) => {
+                        pass = key.toString('base64');
+
+                        var sql = 'SET @token = \'\';'
+                            + 'CALL userlogin(\'' + (req.body.phone === undefined ? req.body.email : req.body.phone) + '\',\''
+                            + pass + '\',' + req.body.loginpath + ', @token);'
+                            + 'SELECT @token;';
+
+                        console.log(sql);
+                        conn.query(sql, function (err, rows) {
+                            if (err) {
+                                res.json('{"resultcode":' + resultcode.Failed + ', "token":""}');
+                                conn.close();
+                                throw err;
+                            }
+                            else {
+                                if (rows[2][0]['@token'] == null) {
+                                    console.log('Password missmatch');
+                                    res.json('{"resultcode":' + resultcode.WorngPassword + ', "token":""}');
+                                    conn.close();
+                                    return;
+                                }
+                                console.log('Issued access token : ' + rows[2][0]['@token']);
+                                res.json('{"resultcode":' + resultcode.Success + ', "token":"' + rows[2][0]['@token'] + '"}');
+                                conn.close();
+                            }
+                        });
+                    });
+                }
+            });
+        }
+        catch (e) {
+            res.json('{"resultcode":' + resultcode.Failed + '}');
+            conn.close();
+            throw err;
+        }
+    });
+    /** 유저 토큰 정상 체크
+     * req : token
+     * res : resultcode 결과값
+     */
+    route.post('/checktoken', function (req, res, next) {
+        console.log(req.body);
+
+        var conn = require('../modules/mysql.js')();
+        try {
+            var pass, salt;
+            var sql = 'CALL usersessioncheck(\'' + req.body.token + '\');';
+
+            conn.query(sql, function (err, rows) {
+                if (err) {
+                    res.json('{"resultcode":' + resultcode.Failed + ', "token":""}');
+                    conn.close();
+                    throw err;
+                }
+                else {
+                    if (rows[0].length <= 0) {
+                        res.json('{"resultcode":' + resultcode.InvalidToken + ', "token":""}');
+                        conn.close();
+                        return;
+                    }
+                    console.log('User alive check : ' + rows[0][0]['userid'] + '_' + rows[0][0]['alive']);
+                    if (rows[0][0]['alive'] == 0) {
+                        res.json('{"resultcode":' + resultcode.ExpiredToken + '}');
+                    }
+                    else {
+                        res.json('{"resultcode":' + resultcode.Success + '}');
+                    }
+                    conn.close();
+                }
+            });
+        }
+        catch (e) {
+            res.json('{"resultcode":' + resultcode.Failed + '}');
+            conn.close();
+            throw err;
         }
     });
     /** 프로필 저장
@@ -65,20 +179,12 @@
         });
     });
     /** 프로필 보기
+     * 주소값에 본인의 이름값(로그인 후 각자 변경가능)전달
      */
     route.get('/profile/:name', function (req, res, next) {
         var key = req.params.name;
 
-        req.redis.get(key, function (err, data) {
-            if (err) {
-                console.log(err);
-                res.send("error " + err);
-                return;
-            }
 
-            var value = JSON.parse(data);
-            res.json(value);
-        });
     });
     // catch 404 and forward to error handler
     route.use(function (req, res, next) {
