20bc12b1f948f6cb76a3475243a54d60b5ff4816
xellossmail <xellossmail@gmail.com>
2018-08-10 11:01:46 +0900

    1

--- NodejsWebApp/modules/common.js
+++ NodejsWebApp/modules/common.js
@@ -2,7 +2,7 @@
     var common = {};
     const fs = require('fs');
     /** 디렉토리 생성
-     * @param {any} dirPath 전체경로를 전달
+     * @param {any} dirPath 전체경로를 전달하여 해당 디렉토리까지 모두 생성
      */
     common.mkdirpath = function(dirPath) {
         if (!fs.existsSync(dirPath)) {
