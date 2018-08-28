d3d4dc5db8ea79db3bce7ea6fd8cfabc297239e3
xellossmail <xellossmail@gmail.com>
2018-08-28 16:20:48 +0900

    문법 정리

--- NodejsWebApp/modules/common.js
+++ NodejsWebApp/modules/common.js
@@ -1,4 +1,4 @@
-﻿module.exports = function () {
+﻿module.exports = ()=>{
   var common = {};
   const fs = require('fs');
   const path = require('path');
@@ -6,7 +6,7 @@
   /** 디렉토리 생성
    * @param {any} dirPath 전체경로를 전달하여 해당 디렉토리까지 모두 생성
    */
-  common.mkdirpath = function(dirPath) {
+  common.mkdirpath = (dirPath)=>{
     if (!fs.existsSync(dirPath)) {
       try {
         fs.mkdirSync(dirPath);
