1ff0fa0cf3ac7baa205b34783bd458790b6a45ee
xellossmail <xellossmail@gmail.com>
2018-09-12 10:08:01 +0900

    semicolons deleted

--- NodejsWebApp/routes/routeTemp.js
+++ NodejsWebApp/routes/routeTemp.js
@@ -2,17 +2,17 @@
  * @param {express} app server sent express object
  */
 module.exports = (app)=>{
-  const express = require('express');
-  const common = require('../modules/common.js')();
-  const fs = require('fs');
-  var resultcode = JSON.parse(fs.readFileSync('resultcode.json', 'utf8').trim());
-  var route = express.Router();
+  const express = require('express')
+  const common = require('../modules/common.js')()
+  const fs = require('fs')
+  var resultcode = JSON.parse(fs.readFileSync('resultcode.json', 'utf8').trim())
+  var route = express.Router()
   // catch 404 and forward to error handler
   route.use((req, res, next)=>{
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
 
-  return route;
-};
+  return route
+}
