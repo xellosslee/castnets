6adbd0845b6b300e5dfb84bfbdc053b14dfa733b
xellossmail <xellossmail@gmail.com>
2018-09-13 16:16:49 +0900

    환경변수에 DB_PORT추가

--- NodejsWebApp/modules/mysql.js
+++ NodejsWebApp/modules/mysql.js
@@ -5,6 +5,7 @@
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     charset: 'utf8',
     multipleStatements: true
   })
