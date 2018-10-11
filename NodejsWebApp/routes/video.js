a56545048ac65da9be47e32a014dd9667bd3f081
xellossmail <xellossmail@gmail.com>
2018-10-11 11:18:06 +0900

    서식변경 및 page변수 변경

--- NodejsWebApp/routes/video.js
+++ NodejsWebApp/routes/video.js
@@ -1,4 +1,4 @@
-﻿module.exports = (app)=>{
+﻿module.exports = (app) => {
   const express = require('express')
   const session = require('express-session')
   const common = require('../modules/common.js')()
@@ -12,79 +12,85 @@
     saveUninitialized: true
   }))
   /**자신 주변의 영상을 가져온다
-   * req : 자신의 위치, 가져올 영상개수, 이전 목록 중 가장 먼 영상의 거리
+   * req : 자신의 위치, 가져올 영상개수, 페이지 넘버(0부터 1씩 증가)
    * res : 해당 범위의 영상 목록 & resultcode {영상 객체는 lat, lon, capturedate, createdate, filepath, distance 값을 가짐}
    */
-  route.get('/list/:lat/:lng/:cnt/:distance', (req, res, next)=>{
+  route.get('/list/:lat/:lng/:cnt/:page', (req, res, next) => {
     const connpool = app.mysqlpool
-    var sql = `CALL videolist('${req.headers.authorization}',${req.params.lat},${req.params.lng},${req.params.cnt},${req.params.distance})`
-    connpool.query(sql, (err, rows)=>{
+    var sql = `CALL videolist('${req.headers.authorization}',${req.params.lat},${req.params.lng},${req.params.cnt},${req.params.page})`
+    connpool.query(sql, (err, rows) => {
       if (err) {
         return next(err)
       }
       //console.log(rows)
       var list = []
       if (rows[0].length > 0) {
-        rows[0].forEach((row)=>{
+        rows[0].forEach((row) => {
           list.push(row)
         })
       }
-      common.sendResult(res, resultcode.Success, {"list": list})
+      common.sendResult(res, resultcode.Success, {
+        "list": list
+      })
     })
   })
   /**지도위의 영상위치를 가져온다
    * req : 현재 표시되는 지도의 시작지점(좌측 위)과 끝지점(우측 아래)의 위경도
    * res : 해당 범위의 영상 목록 & resultcode {영상 객체는 lat, lon, capturedate, createdate, filepath 값을 가짐}
    */
-  route.get('/maplist/:slat/:slng/:elat/:elng', (req, res, next)=>{
+  route.get('/maplist/:slat/:slng/:elat/:elng', (req, res, next) => {
     const connpool = app.mysqlpool
-    connpool.query('CALL videomaplist(' + req.params.slat + ',' + req.params.slng + ',' + req.params.elat + ',' + req.params.elng + ')', (err, rows)=>{
+    connpool.query('CALL videomaplist(' + req.params.slat + ',' + req.params.slng + ',' + req.params.elat + ',' + req.params.elng + ')', (err, rows) => {
       if (err) {
         return next(err)
       }
       //console.log(rows)
       var list = []
       if (rows[0].length > 0) {
-        rows[0].forEach((row)=>{
+        rows[0].forEach((row) => {
           list.push(row)
         })
       }
-      common.sendResult(res, resultcode.Success, {"list": list})
+      common.sendResult(res, resultcode.Success, {
+        "list": list
+      })
     })
   })
   /**특정 영상 주변의 영상을 가져온다
-   * req : 보여질 영상의 번호, 자신의 위치, 가져올 영상개수, 이전 목록 중 가장 먼 영상의 거리
+   * req : 보여질 영상의 번호, 자신의 위치, 가져올 영상개수, 페이지 넘버(0부터 1씩 증가)
    * res : 해당 범위의 영상 목록 & resultcode {영상 객체는 lat, lon, capturedate, createdate, filepath, distancefromme, distance 값을 가짐}
    */
-  route.get('/targetlist/:videoid/:lat/:lng/:cnt/:distance', (req, res, next)=>{
+  route.get('/targetlist/:videoid/:lat/:lng/:cnt/:page', (req, res, next) => {
     const connpool = app.mysqlpool
-    connpool.query('CALL videotargetlist(' + req.params.videoid + ',' + req.params.lat + ',' + req.params.lng + ',' + req.params.cnt + ',' + req.params.distance + ')', (err, rows)=>{
+    connpool.query('CALL videotargetlist(' + req.params.videoid + ',' + req.params.lat + ',' + req.params.lng + ',' + req.params.cnt + ',' + req.params.page + ')', (err, rows) => {
       if (err) {
         return next(err)
       }
       // console.log(rows)
       var list = []
       if (rows[0].length > 0) {
-        rows[0].forEach((row)=>{
+        rows[0].forEach((row) => {
           list.push(row)
         })
       }
       if (rows[1].length > 0) {
-        rows[1].forEach((row)=>{
+        rows[1].forEach((row) => {
           list.push(row)
         })
       }
-      common.sendResult(res, resultcode.Success, {"list": list})
+      common.sendResult(res, resultcode.Success, {
+        "list": list
+      })
     })
   })
   /**비디오 영상플레이 기록
    * req : videoid, token(존재하지 않으면 미전달), logtype 영상입장 70101, 영상완료(끝까지 재생) 70102, 영상퇴장 70103
    * res : 결과 없음. 성공여부에 상관없이 진행
    */
-  route.post('/view', (req, res, next)=>{
+  route.post('/view', (req, res, next) => {
     const connpool = app.mysqlpool
     var sql = `CALL videoview(${req.body.videoid},'${req.headers.authorization}',${req.body.logtype})`
-    connpool.query(sql, (err, rows)=>{
+    connpool.query(sql, (err, rows) => {
       if (err) {
         return next(err)
       }
@@ -95,10 +101,10 @@
    * req : html5에서는 플레이어에서 각종 정보를 보내줌. 앱에서는 특정 videoid 값을 보내야함
    * res : 해당 영상 내용을 리턴
    */
-  route.get('/stream/:videoid', (req, res, next)=>{
+  route.get('/stream/:videoid', (req, res, next) => {
     const connpool = app.mysqlpool
     var sql = `CALL videostream(${req.params.videoid})`
-    connpool.query(sql, (err, rows)=>{
+    connpool.query(sql, (err, rows) => {
       try {
         console.log(`video stream[${req.params.videoid}]`)
         if (err) {
@@ -116,24 +122,23 @@
             try {
               var stat1 = fs.statSync(filepath)
               console.log(stat1)
-            }
-            catch (err) {
+            } catch (err) {
               console.log('testlog - ' + err)
             }
           }
-          
+
           if (req.headers['range']) {
             var range = req.headers.range
             var parts = range.replace(/bytes=/, "").split("-")
             var partialstart = parts[0]
             var partialend = parts[1]
-  
+
             var start = parseInt(partialstart, 10)
             var end = partialend ? parseInt(partialend, 10) : total - 1
             var chunksize = (end - start) + 1
             console.log(`reqRange : ${req.headers.range}`)
             console.log(`resRange : ${start} - ${end} = ${chunksize}`)
-  
+
             var file = fs.createReadStream(path, {
               start: start,
               end: end
@@ -154,8 +159,7 @@
             fs.createReadStream(path).pipe(res)
           }
         }
-      }
-      catch(err) { // 스트리밍 과정에서 오류가 나는 경우가 있음
+      } catch (err) { // 스트리밍 과정에서 오류가 나는 경우가 있음
         if (err) {
           return next(err)
         }
@@ -166,7 +170,7 @@
    * req : videoid를 주소에 담아서 전달
    * res : 해당 영상의 썸네일을 리턴
    */
-  route.get('/thumbnail/:videoid', (req, res, next)=>{
+  route.get('/thumbnail/:videoid', (req, res, next) => {
     const connpool = app.mysqlpool
     var sql = `CALL videothumbnail('${req.params.videoid}')`
 
