41e5f71fc085edf9a914c289edfe945e346a39a4
xellossmail <xellossmail@gmail.com>
2018-10-11 13:31:42 +0900

    코드 변경

--- config/code insert.sql
+++ config/code insert.sql
@@ -13,10 +13,11 @@ INSERT INTO `code` VALUES (10205, 10200, '차단유저', '차단유저');
 
 INSERT INTO `code` VALUES (30000, NULL, '영상정보', '영상정보');
 INSERT INTO `code` VALUES (30100, 30000, '영상종류', '미설정');
-INSERT INTO `code` VALUES (30101, 30100, '영상종류', '음식');
-INSERT INTO `code` VALUES (30102, 30100, '영상종류', '여행');
-INSERT INTO `code` VALUES (30103, 30100, '영상종류', '인물');
-INSERT INTO `code` VALUES (30104, 30100, '영상종류', '동물');
+INSERT INTO `code` VALUES (30101, 30100, '영상종류', '방송');
+INSERT INTO `code` VALUES (30102, 30100, '영상종류', '학습');
+INSERT INTO `code` VALUES (30103, 30100, '영상종류', '음악');
+INSERT INTO `code` VALUES (30104, 30100, '영상종류', '음식');
+INSERT INTO `code` VALUES (30105, 30100, '영상종류', '개인');
 
 INSERT INTO `code` VALUES (60000, NULL, '파일정보', '파일정보');
 INSERT INTO `code` VALUES (60100, 60000, '등록위치', '미설정');
